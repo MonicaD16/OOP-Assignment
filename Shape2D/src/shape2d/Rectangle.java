276ad7094fa66e647462a9c0926f677ca06757bf
Monica <mdiep1@ocdsb.ca>
2018-04-08 20:27:40 -0400

    Allow user input

--- Shape2D/src/shape2d/Rectangle.java
+++ Shape2D/src/shape2d/Rectangle.java
@@ -4,11 +4,7 @@ package shape2d;
 public class Rectangle extends Shape2D {
     private final double length, width; 
 
-    public Rectangle() {
-       
-        this(1,1);
-   
-    }
+
     public Rectangle(double width, double length) {
         this.width = width;
         this.length = length;
--- Shape2D/src/shape2d/ShapeMain.java
+++ Shape2D/src/shape2d/ShapeMain.java
@@ -1,22 +1,41 @@
 
 package shape2d;
 
+import java.util.Scanner;
+
 
 public class ShapeMain {
     public static void main(String[] args) {
-        double width = 5, length = 7;
+        
+        Scanner widthInput = new Scanner(System.in);
+        System.out.println("Enter the width of the rectangle: ");
+        double width = widthInput.nextInt();
+        
+        System.out.println("Enter the length of the rectangle: ");
+        double length = widthInput.nextInt();
+        
         Shape2D rectangle = new Rectangle(width, length);
         System.out.println("Rectangle width: " + width + " and length: " + length
                 + "\nArea of rectangle: " + rectangle.area()
                 + "\nPerimeter of rectangle: " + rectangle.perimeter() + "\n");
 
-        double radius = 5;
+        System.out.println("Enter the radius of the circle. ");
+        double radius = widthInput.nextInt();
+        
         Shape2D circle = new Circle(radius);
         System.out.println("Circle radius: " + radius
             + "\nArea of circle: " + circle.area()
             + "\nPerimeter of circle: " + circle.perimeter() + "\n");
 
-        double sideA = 5, sideB = 3, sideC = 4;
+        System.out.println("Enter side A of the triangle: ");
+        double sideA = widthInput.nextInt();
+        
+        System.out.println("Enter side B of the triangle: ");
+        double sideB = widthInput.nextInt();
+        
+        System.out.println("Enter side C of the triangle: ");
+        double sideC = widthInput.nextInt();
+        
         Shape2D triangle = new Triangle(sideA, sideB, sideC);
         System.out.println("Triangle sides lengths: " + sideA + ", " + sideB + ", " + sideC
                 + "\nArea of triangle: " + triangle.area()
